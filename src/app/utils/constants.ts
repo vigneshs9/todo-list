36314883e8196821a4ccb6210321a2290da6ba33
vignesh <vigneshsuthapalli@gmail.com>
2026-05-01 13:13:55 +0530

    Changes

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment'
 
 @Injectable({
  providedIn: 'root',
 })
 export class Constants {
- static readonly BASE_URL = 'http://localhost:3000/';
+ static readonly BASE_URL = environment.apiUrl;
  static readonly LOGIN_ENDPOINT = Constants.BASE_URL + 'login';
  static readonly SIGNUP_ENDPOINT = Constants.LOGIN_ENDPOINT + '/signup';
  static readonly CHANGE_PASSWORD_ENDPOINT = Constants.LOGIN_ENDPOINT + '/changePassword';
