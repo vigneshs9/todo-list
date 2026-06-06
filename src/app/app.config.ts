2bba1281116fe3f39e68d591b4c32bb7870ddc9a
vignesh <vigneshsuthapalli@gmail.com>
2026-06-06 15:01:43 +0530

    Implement encryption service and update API manager for encrypted requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth-interceptor';
+import { authInterceptor } from './services/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
  providers: [
--- src/app/utils/api-manager.ts
+++ src/app/utils/api-manager.ts
@@ -1,24 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { EncryptionService } from '../services/encryption.service';
+import { map } from 'rxjs';
 
 @Injectable({
  providedIn: 'root',
 })
 export class ApiManager {
  private readonly http = inject(HttpClient);
+ private readonly crypto = inject(EncryptionService);
  doPost(url: string, body: any) {
-  return this.http.post(url, body);
+  return this.http.post(url, this.crypto.encryptedPayload(body)).pipe(map((res: any) => this.crypto.decrypt(res?.['payload'])));
  }
  doPut(url: string, body: any) {
-  return this.http.put(url, body);
+  return this.http.put(url, this.crypto.encryptedPayload(body)).pipe(map((res: any) => this.crypto.decrypt(res?.['payload'])));
  }
  doDelete(url: string, body: any) {
-  return this.http.delete(url, body);
+  return this.http.delete(url, this.crypto.encryptedPayload(body)).pipe(map((res: any) => this.crypto.decrypt(res?.['payload'])));
  }
  doGet(url: string) {
-  return this.http.get(url);
+  return this.http.get(url).pipe(map((res: any) => this.crypto.decrypt(res?.['payload'])));
  }
  uploadFile(url: string, file: File) {
-  return this.http.put(url, file, { headers: { 'Content-Type': file.type } });
+  return this.http.put(url, this.crypto.encryptedPayload(file), { headers: { 'Content-Type': file.type } }).pipe(map((res: any) => this.crypto.decrypt(res?.['payload'])));
  }
 }
